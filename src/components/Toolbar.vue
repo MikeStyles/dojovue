0d3dc2623f2ba94478cb9cc090c23bd092e025c4
mikestyles <duvankno3@gmail.com>
2020-09-19 09:59:54 -0500

    commit

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-app-bar
+      color="dark lighten-34"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Galeria dojo vue</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Toolbar'
+}
+</script>
